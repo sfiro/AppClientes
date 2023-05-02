94ccbf099202678795573c996977481eaa0d92f4
Debbie Arredondo <chevi6@gmail.com>
2023-05-02 00:00:18 -0500

    se crea el metodo list, que permite imprimir los datos guardados en el CSV

--- clients/commands.py
+++ clients/commands.py
@@ -38,8 +38,21 @@ def create(ctx,name,company,email,position):
 @click.pass_context
 def list(ctx):
     """list all clients"""
-    pass
+    Client_service = ClientService(ctx.obj['clients_table'])
 
+    client_list =  Client_service.list_clients()
+
+    click.echo("  ID   |  NAME  |  COMPANY  |  EMAIL  |  POSITION")
+    click.echo( "*"* 100)
+
+    for client in client_list:
+        click.echo("  {uid}  |  {name}  |  {company}  |  {email}  |  {position}  ".format(
+            uid = client['uid'],
+            name = client['name'],
+            company = client['company'],
+            email = client['email'],
+            position = client['position']
+        ))
 
 @clients.command()
 @click.pass_context
--- clients/services.py
+++ clients/services.py
@@ -11,3 +11,8 @@ class ClientService:
         with open(self.table_name,mode='a') as f:
             writer = csv.DictWriter(f,fieldnames=Client.schema())
             writer.writerow(cliente.to_dict())
+
+    def list_clients(self):
+        with open(self.table_name,mode='r') as f:
+            reader = csv.DictReader(f,fieldnames=Client.schema())
+            return list(reader)
